9185544ae8fd81694eade3060ed7b3973ebb71b7
Lubos Lehota <lubos.lehota@cleverlance.com>
2024-12-26 18:32:58 -0300

    Create empty project

---
+++ GymNote/GymNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymNoteApp.swift
+//  GymNote
+//
+//  Created by Lubos Lehota on 26/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct GymNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
